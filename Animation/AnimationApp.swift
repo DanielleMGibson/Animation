d3d9db0f8c397f8f01af500ec984b9ed9298b970
Danielle Gibson <peyton.gibson@stu.barren.kyschools.us>
2024-11-26 08:02:10 -0600

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Student on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
